f75ff432b86cb0b8e9de6d678631e5bcb5881c7d
Nill Mefistophell <asklepiy@i.ua>
2014-12-21 13:12:34 +0200

    #

--- frontend/modules/video/Bootstrap.php
+++ frontend/modules/video/Bootstrap.php
@@ -7,31 +7,28 @@ use yii\base\BootstrapInterface;
 /**
  * Gallery module bootstrap class.
  */
-class Bootstrap implements BootstrapInterface
-{
+class Bootstrap implements BootstrapInterface {
+
     /**
      * @inheritdoc
      */
-    public function bootstrap($app)
-    {
+    public function bootstrap($app) {
         // Add module URL rules.
         $app->getUrlManager()->addRules(
-            [
-                'video' => 'video/video/index',
-                'video/<_a:(create|contacts|captcha)>' => 'video/video/<_a>'
-            ]
+                [
+                    'video' => 'video/video/index',
+                    'video/<_a:(create|contacts|captcha)>' => 'video/video/<_a>'
+                ]
         );
 
         // Add module I18N category.
-        
-            $app->i18n->translations['ru*'] = [
-                'class' => 'yii\i18n\PhpMessageSource',
-                'basePath' => '@app/messages',
-                'forceTranslation' => true,
-                'fileMap' => [
-                    'ru' => 'ru.php',
-                ]
-            ];
-        
+        $app->i18n->translations['ru*'] = [
+            'class' => 'yii\i18n\PhpMessageSource',
+            'basePath' => '@app/messages',
+            'forceTranslation' => true,
+            'fileMap' => [
+                'ru' => 'ru.php',
+            ]
+        ];
     }
 }
