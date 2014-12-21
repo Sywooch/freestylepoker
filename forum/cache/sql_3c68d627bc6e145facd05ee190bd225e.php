a396ab95319807f4debc12c943610a81dac495d8
Nill Mefistophell <asklepiy@i.ua>
2014-12-21 13:31:20 +0200

    Forum Styles Update

--- forum/cache/sql_3c68d627bc6e145facd05ee190bd225e.php
+++
@@ -1,5 +0,0 @@
-<?php exit; ?>
-1418930811
-SELECT * FROM frm_styles s WHERE s.style_id = 3
-283
-a:1:{i:0;a:8:{s:8:"style_id";s:1:"3";s:10:"style_name";s:9:"prosilver";s:15:"style_copyright";s:20:"© phpBB Group, 2007";s:12:"style_active";s:1:"1";s:10:"style_path";s:9:"prosilver";s:15:"bbcode_bitfield";s:4:"lNg=";s:15:"style_parent_id";s:1:"0";s:17:"style_parent_tree";s:0:"";}}
