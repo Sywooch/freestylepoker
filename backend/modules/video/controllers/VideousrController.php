5ba4f9e5658d6f3a6a9ad99e726b4dbd013ad64a
Nill Mefistophell <asklepiy@i.ua>
2014-12-23 12:00:45 +0200

    add author

---
+++ backend/modules/video/controllers/VideousrController.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace app\modules\video\controllers;
+
+use Yii;
+use app\modules\video\models\VideoUsr;
+use app\modules\video\models\VideoUsrSearch;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+
+/**
+ * VideoUsrController implements the CRUD actions for VideoUsr model.
+ */
+class VideousrController extends Controller
+{
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['post'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * Lists all VideoUsr models.
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        $searchModel = new VideoUsrSearch();
+        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+    /**
+     * Displays a single VideoUsr model.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionView($id)
+    {
+        return $this->render('view', [
+            'model' => $this->findModel($id),
+        ]);
+    }
+
+    /**
+     * Creates a new VideoUsr model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     * @return mixed
+     */
+    public function actionCreate()
+    {
+        $model = new VideoUsr();
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->id]);
+        } else {
+            return $this->render('create', [
+                'model' => $model,
+            ]);
+        }
+    }
+
+    /**
+     * Updates an existing VideoUsr model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionUpdate($id)
+    {
+        $model = $this->findModel($id);
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->id]);
+        } else {
+            return $this->render('update', [
+                'model' => $model,
+            ]);
+        }
+    }
+
+    /**
+     * Deletes an existing VideoUsr model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionDelete($id)
+    {
+        $this->findModel($id)->delete();
+
+        return $this->redirect(['index']);
+    }
+    
+    /**
+     * Delete multiple posts page.
+     *
+     * @return mixed
+     * @throws \yii\web\HttpException
+     */
+    public function actionBatchDelete()
+    {
+        if (($ids = Yii::$app->request->post('ids')) !== null) {
+            $models = $this->findModel($ids);
+            foreach ($models as $model) {
+                $model->delete();
+            }
+            return $this->redirect(['index']);
+        } else {
+            throw new HttpException(400);
+        }
+    }
+
+    /**
+     * Finds the VideoUsr model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param integer $id
+     * @return VideoUsr the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (is_array($id)) {
+            $model = VideoUsr::findAll($id);
+        } else {
+            $model = VideoUsr::findOne($id);
+        }
+        if ($model !== null) {
+            return $model;
+        } else {
+            throw new HttpException(404);
+        }
+    }
+}
