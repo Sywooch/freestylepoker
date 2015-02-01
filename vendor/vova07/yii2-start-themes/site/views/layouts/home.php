36b7fca43f07e013789d0236e62512abea5a7e42
Nill Mefistophell <asklepiy@i.ua>
2015-02-01 23:38:03 +0200

    CSS Ext

--- vendor/vova07/yii2-start-themes/site/views/layouts/home.php
+++ vendor/vova07/yii2-start-themes/site/views/layouts/home.php
@@ -6,58 +6,61 @@
  * @var \yii\web\View $this View
  * @var string $content Content
  */
-
 use vova07\themes\site\widgets\Alert;
-
 ?>
 <?php $this->beginPage(); ?>
-    <!DOCTYPE html>
-    <html lang="<?= Yii::$app->language ?>">
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
     <head>
         <?= $this->render('//layouts/head') ?>
     </head>
     <body>
-    <?php $this->beginBody(); ?>
+        <?php $this->beginBody(); ?>
 
-    <header class="navbar navbar-inverse navbar-fixed-top wet-asphalt" role="banner">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                    <span class="sr-only"><?= Yii::t('vova07/themes/site', 'Toggle navigation') ?></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="<?= Yii::$app->homeUrl ?>">
-                    <?= Yii::$app->name ?>
-                </a>
-            </div>
-            <div class="collapse navbar-collapse">
-                <?= $this->render('//layouts/top-menu') ?>
-            </div>
-        </div>
-    </header>
-    <!--/header-->
-
-    <?= Alert::widget(); ?>
-
-    <?= $content ?>
-
-    <footer id="footer" class="midnight-blue">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-6">
-                    &copy; 2014 <?= Yii::$app->name ?>. <?= Yii::t('vova07/themes/site', 'All Rights Reserved') ?>.
-                </div>
-                <div class="col-sm-6">
-                    <?= $this->render('//layouts/top-menu', ['footer' => true]) ?>
+        <header class="navbar navbar-inverse navbar-fixed-top wet-asphalt" role="banner">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only"><?= Yii::t('vova07/themes/site', 'Toggle navigation') ?></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="<?= Yii::$app->homeUrl ?>">
+                        <?= Yii::$app->name ?>
+                    </a>
                 </div>
             </div>
-        </div>
-    </footer>
-    <!--/#footer-->
+        </header>
+        <!--/header-->
 
-    <?php $this->endBody(); ?>
+        <section id="title" class="emerald">
+            <div class="container">
+                <div class="row">
+                    <?= $this->render('//layouts/top-menu') ?>
+                </div>
+            </div>
+        </section><!--/#title-->
+
+        <?= Alert::widget(); ?>
+
+        <?= $content ?>
+
+        <footer id="footer" class="midnight-blue">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6">
+                        &copy; 2014 <?= Yii::$app->name ?>. <?= Yii::t('vova07/themes/site', 'All Rights Reserved') ?>.
+                    </div>
+                    <div class="col-sm-6">
+                        <?= $this->render('//layouts/top-menu', ['footer' => true]) ?>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!--/#footer-->
+
+        <?php $this->endBody(); ?>
     </body>
-    </html>
+</html>
 <?php $this->endPage(); ?>
