d1d22cdb0801b4da677560014071dd8e53c42645
Nill Mefistophell <asklepiy@i.ua>
2015-02-27 12:56:22 +0200

    Promo

--- frontend/modules/rooms/views/roomspromo/index.php
+++ frontend/modules/rooms/views/roomspromo/index.php
@@ -12,24 +12,42 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="rooms-promo-index">
 
-    <h1><?= Html::encode($this->title) ?></h1>
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+    <h4> <?= Html::img(Yii::$app->assetManager->publish('@vova07/themes/site/assets/images/rooms.png')[1], ['class' => 'section_logo']) ?>
+        <?= Html::encode($this->title) ?></h4>
+    <p>
+        Здесь собраны все акции покер-румов
+    </p>
 
     <?=
     GridView::widget([
         'dataProvider' => $dataProvider,
-        'columns' => [
-            [
-                'attribute' => 'img',
-                'format' => 'html',
-                'value' => function ($model) {
-                    return Html::a(Html::img('/statics/web/rooms/previews/'.$model->img, ['style'=>'width:100px']), $model->alias.'/');
+        'layout' => "{items}",
+        'rowOptions' => function ($model) {
+            return ['class' => 'roomspromo_row'];
+        },
+                'showHeader' => false,
+                'tableOptions' => ['class' => 'trainings_table'],
+                'columns' => [
+                    [
+                        'attribute' => 'img',
+                        'format' => 'raw',
+                        'contentOptions' => ['class' => 'roomspromo_row_img'],
+                        'value' => function ($model) {
+                    return Html::a(Html::img('/statics/web/rooms/previews/' . $model->img, ['style' => 'width:100px']), $model->alias . '/');
                 }
-            ],
-            'name',
-            'text:ntext',
-        ],
-    ]);
-    ?>
+                    ],
+                    [
+                        'attribute' => 'name',
+                        'format' => 'raw',
+                        'contentOptions' => ['class' => 'roomspromo_row_name'],
+                    ],
+                    [
+                        'attribute' => 'text',
+                        'format' => 'raw',
+                        'contentOptions' => ['class' => 'roomspromo_row_text'],
+                    ],
+                ],
+            ]);
+            ?>
 
 </div>
